267df21daba3137416d2b5829a71c0b71e1bb7a5
Aditya Thakur <taditiya870@gmail.com>
2024-09-06 16:44:37 +0530

    corrected the program
    not added the height variable in cone_vol.c.

--- C_language/perimeter, area_&_volume/cone_vol.c
+++ C_language/perimeter, area_&_volume/cone_vol.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main() {
-    float radius;
+    float radius,height;
     printf("Enter the radius of the cone: ");
     scanf("%f", &radius);
+    printf("Enter the height of the cone: ");
+    scanf("%f", &height);
     printf("Volume of the cone is: %f\n", (3.14 * radius*radius*height)/3);
     return 0;
 }
