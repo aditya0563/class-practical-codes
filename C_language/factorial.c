a347f76b767fd0d3c7e7a1836df922be8ffc7e8b
Aditya Thakur <taditiya870@gmail.com>
2024-09-20 22:33:29 +0530

    can find the factorial of a number
    in this commit, i have written the code by which we can find the factorial of any number by inputing .

---
+++ C_language/factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num, fact = 1;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++) {
+        fact = fact * i;
+    }
+    printf("Factorial of %d is %d\n", num, fact);
+    return 0;
+}
